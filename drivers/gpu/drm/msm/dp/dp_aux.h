f3fb5b078f69529067bc5f6b462e33a3e35c535b
Chandan Uddaraju <chandanu@codeaurora.org>
2020-03-31 17:30:29 -0700

    FROMLIST: drm/msm/dp: add displayPort driver support
    Add the needed displayPort files to enable DP driver
    on msm target.
    
    "dp_display" module is the main module that calls into
    other sub-modules. "dp_drm" file represents the interface
    between DRM framework and DP driver.
    
    changes in v2:
    -- Update copyright markings on all relevant files.
    -- Change pr_err() to DRM_ERROR()
    -- Use APIs directly instead of function pointers.
    -- Use drm_display_mode structure to store link parameters.
    -- Use macros for register definitions instead of hardcoded values.
    -- Replace writel_relaxed/readl_relaxed with writel/readl
       and remove memory barriers.
    -- Remove unnecessary NULL checks.
    -- Use drm helper functions for dpcd read/write.
    -- Use DRM_DEBUG_DP for debug msgs.
    
    changes in V3:
    -- Removed changes in dpu_io_util.[ch]
    -- Added locking around "is_connected" flag and removed atomic_set()
    -- Removed the argument validation checks in all the static functions
       except initialization functions and few API across msm/dp files.
    -- Removed hardcoded values for register reads/writes
    -- Removed vreg related generic structures.
    -- Added return values where ever necessary.
    -- Updated dp_ctrl_on function.
    -- Calling the ctrl specific catalog functions directly instead of
       function pointers.
    -- Added separate change that adds standard value in drm_dp_helper file.
    -- Added separate change in this list that is used to initialize DP.
    -- Added change to use drm_dp_get_adjust_request_voltage() function.
    
    changes in V4:
    -- Fix mvid, nvid calculation.
    -- Shift video timings to align to bottom right.
    -- Fix register offsets for pixel_m and pixel_n.
    -- Fix TPG programming sequence.
    -- Update DP ack override register to recommended value.
    -- Added code to log displayport registers.
    -- Update aux setup with recommended settings.
    -- Fix TU calculations for all resolutions.
    -- Limit linkrate to HBR2 until HBR3 support is added.
    -- Update DPCD read sequence.
    -- Update code comments in dp_power_init() of V3.
    -- Remove extcon dependencies.
    -- Move drm helper functions locally.
    
    changes in V5:
    -- Addressed comments from Jordan.
    
    Signed-off-by: Chandan Uddaraju <chandanu@codeaurora.org>
    Signed-off-by: Vara Reddy <varar@codeaurora.org>
    (am from https://patchwork.kernel.org/patch/11468715/)
    (also found at https://lkml.kernel.org/r/1585701031-28871-4-git-send-email-tanmay@codeaurora.org)
    
    BUG=b:148864048
    TEST=Connect DP cable to type-c ports, see secondary display output, try
         various type-c to HDMI and DP dongles on Trogdor
    
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Change-Id: I2e762be530398c8a423b6ee17bfe41d4fa16bf35

---
+++ drivers/gpu/drm/msm/dp/dp_aux.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DP_AUX_H_
+#define _DP_AUX_H_
+
+#include "dp_catalog.h"
+#include <drm/drm_dp_helper.h>
+
+#define DP_AUX_ERR_NONE		0
+#define DP_AUX_ERR_ADDR		-1
+#define DP_AUX_ERR_TOUT		-2
+#define DP_AUX_ERR_NACK		-3
+#define DP_AUX_ERR_DEFER	-4
+#define DP_AUX_ERR_NACK_DEFER	-5
+
+struct dp_aux {
+	struct drm_dp_aux *drm_aux;
+	struct dp_aux_cfg *cfg;
+};
+
+int dp_aux_register(struct dp_aux *aux);
+void dp_aux_unregister(struct dp_aux *aux);
+void dp_aux_isr(struct dp_aux *aux);
+void dp_aux_init(struct dp_aux *aux, struct dp_aux_cfg *aux_cfg);
+void dp_aux_deinit(struct dp_aux *aux);
+void dp_aux_reconfig(struct dp_aux *aux);
+
+struct dp_aux *dp_aux_get(struct device *dev, struct dp_catalog *catalog,
+			  struct dp_aux_cfg *aux_cfg);
+void dp_aux_put(struct dp_aux *aux);
+
+#endif /*__DP_AUX_H_*/
