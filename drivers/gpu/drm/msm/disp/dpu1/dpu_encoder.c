131be7c0885f57fa339c56a283d7d3a5b6fb06ed
Chandan Uddaraju <chandanu@codeaurora.org>
2020-03-31 17:30:29 -0700

    FROMLIST: drm/msm/dp: add displayPort driver support
    Add the needed displayPort files to enable DP driver
    on msm target.
    
    "dp_display" module is the main module that calls into
    other sub-modules. "dp_drm" file represents the interface
    between DRM framework and DP driver.
    
    changes in v2:
    -- Update copyright markings on all relevant files.
    -- Change pr_err() to DRM_ERROR()
    -- Use APIs directly instead of function pointers.
    -- Use drm_display_mode structure to store link parameters.
    -- Use macros for register definitions instead of hardcoded values.
    -- Replace writel_relaxed/readl_relaxed with writel/readl
       and remove memory barriers.
    -- Remove unnecessary NULL checks.
    -- Use drm helper functions for dpcd read/write.
    -- Use DRM_DEBUG_DP for debug msgs.
    
    changes in V3:
    -- Removed changes in dpu_io_util.[ch]
    -- Added locking around "is_connected" flag and removed atomic_set()
    -- Removed the argument validation checks in all the static functions
       except initialization functions and few API across msm/dp files.
    -- Removed hardcoded values for register reads/writes
    -- Removed vreg related generic structures.
    -- Added return values where ever necessary.
    -- Updated dp_ctrl_on function.
    -- Calling the ctrl specific catalog functions directly instead of
       function pointers.
    -- Added separate change that adds standard value in drm_dp_helper file.
    -- Added separate change in this list that is used to initialize DP.
    -- Added change to use drm_dp_get_adjust_request_voltage() function.
    
    changes in V4:
    -- Fix mvid, nvid calculation.
    -- Shift video timings to align to bottom right.
    -- Fix register offsets for pixel_m and pixel_n.
    -- Fix TPG programming sequence.
    -- Update DP ack override register to recommended value.
    -- Added code to log displayport registers.
    -- Update aux setup with recommended settings.
    -- Fix TU calculations for all resolutions.
    -- Limit linkrate to HBR2 until HBR3 support is added.
    -- Update DPCD read sequence.
    -- Update code comments in dp_power_init() of V3.
    -- Remove extcon dependencies.
    -- Move drm helper functions locally.
    
    changes in V5:
    -- Addressed comments from Jordan.
    
    Signed-off-by: Chandan Uddaraju <chandanu@codeaurora.org>
    Signed-off-by: Vara Reddy <varar@codeaurora.org>
    (am from https://patchwork.kernel.org/patch/11468715/)
    (also found at https://lkml.kernel.org/r/1585701031-28871-4-git-send-email-tanmay@codeaurora.org)
    
    BUG=b:148864048
    TEST=Connect DP cable to type-c ports, see secondary display output, try
         various type-c to HDMI and DP dongles on Trogdor
    
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Change-Id: I2e762be530398c8a423b6ee17bfe41d4fa16bf35

--- drivers/gpu/drm/msm/disp/dpu1/dpu_encoder.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_encoder.c
@@ -992,6 +992,9 @@ static void dpu_encoder_virt_mode_set(struct drm_encoder *drm_enc,
 
 	trace_dpu_enc_mode_set(DRMID(drm_enc));
 
+	if (drm_enc->encoder_type == DRM_MODE_ENCODER_TMDS && priv->dp)
+		msm_dp_display_mode_set(priv->dp, drm_enc, mode, adj_mode);
+
 	list_for_each_entry(conn_iter, connector_list, head)
 		if (conn_iter->encoder == drm_enc)
 			conn = conn_iter;
@@ -1134,6 +1137,7 @@ static void dpu_encoder_virt_enable(struct drm_encoder *drm_enc)
 {
 	struct dpu_encoder_virt *dpu_enc = NULL;
 	int ret = 0;
+	struct msm_drm_private *priv;
 	struct drm_display_mode *cur_mode = NULL;
 
 	if (!drm_enc) {
@@ -1144,6 +1148,7 @@ static void dpu_encoder_virt_enable(struct drm_encoder *drm_enc)
 
 	mutex_lock(&dpu_enc->enc_lock);
 	cur_mode = &dpu_enc->base.crtc->state->adjusted_mode;
+	priv = drm_enc->dev->dev_private;
 
 	trace_dpu_enc_enable(DRMID(drm_enc), cur_mode->hdisplay,
 			     cur_mode->vdisplay);
@@ -1164,6 +1169,15 @@ static void dpu_encoder_virt_enable(struct drm_encoder *drm_enc)
 
 	_dpu_encoder_virt_enable_helper(drm_enc);
 
+	if (drm_enc->encoder_type == DRM_MODE_ENCODER_TMDS && priv->dp) {
+		ret = msm_dp_display_enable(priv->dp,
+						drm_enc);
+		if (ret) {
+			DPU_ERROR_ENC(dpu_enc, "dp display enable failed: %d\n",
+				ret);
+			goto out;
+		}
+	}
 	dpu_enc->enabled = true;
 
 out:
@@ -1196,6 +1210,12 @@ static void dpu_encoder_virt_disable(struct drm_encoder *drm_enc)
 	dpu_kms = to_dpu_kms(priv->kms);
 	global_state = dpu_kms_get_existing_global_state(dpu_kms);
 
+	if (drm_enc->encoder_type == DRM_MODE_ENCODER_TMDS && priv->dp) {
+		if (msm_dp_display_disable(priv->dp, drm_enc)) {
+			DPU_ERROR_ENC(dpu_enc, "dp display disable failed\n");
+			return;
+		}
+	}
 	trace_dpu_enc_disable(DRMID(drm_enc));
 
 	/* wait for idle */
