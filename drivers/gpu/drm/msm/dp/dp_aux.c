7d592c525b1cb8c2aee1b6ab42ff8697eee2ef2a
Kuogee Hsieh <khsieh@codeaurora.org>
2020-08-18 16:09:00 -0700

    drm/msm/dp: Add DP compliance tests on Snapdragon Chipsets
    add event thread to execute events serially from event queue. Also
    timeout mode is supported  which allow an event be deferred to be
    executed at later time. Both link and phy compliant tests had been
    done successfully.
    
    Changes in v2:
    - Fix potential deadlock by removing redundant connect_mutex
    - Check and enable link clock during modeset
    - Drop unused code and fix function prototypes.
    - set sink power to normal operation state (D0) before DPCD read
    
    Changes in v3:
    - push idle pattern at main link before timing generator off
    - add timeout handles for both connect and disconnect
    
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    
    This patch depends-on following series:
    https://patchwork.kernel.org/project/dri-devel/list/?series=334239
    
    This patch depends-on following series:
    https://patchwork.kernel.org/project/dri-devel/list/?series=334239

--- drivers/gpu/drm/msm/dp/dp_aux.c
+++ drivers/gpu/drm/msm/dp/dp_aux.c
@@ -189,6 +189,8 @@ static void dp_aux_native_handler(struct dp_aux_private *aux)
 		aux->aux_error_num = DP_AUX_ERR_TOUT;
 	if (isr & DP_INTR_NACK_DEFER)
 		aux->aux_error_num = DP_AUX_ERR_NACK;
+	if (isr & DP_INTR_AUX_ERROR)
+		aux->aux_error_num = DP_AUX_ERR_DPPHY_AUX;
 
 	complete(&aux->comp);
 }
@@ -359,6 +361,7 @@ static ssize_t dp_aux_transfer(struct drm_dp_aux *dp_aux,
 					PHY_AUX_CFG1);
 			dp_catalog_aux_reset(aux->catalog);
 		}
+		usleep_range(400, 500); /* at least 400us to next try */
 		goto unlock_exit;
 	}
 
