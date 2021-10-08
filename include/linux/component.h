213b19ceb3f4cd045e70738fcf4022049ce51df6
Stephen Boyd <swboyd@chromium.org>
2021-10-07 17:00:16 -0700

    component: Remove most references to 'master'
    Remove most references to 'master' in the code now that we've decided to
    migrate all the users of the ops structure to the aggregate driver.
    
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Cc: Rob Clark <robdclark@gmail.com>
    Cc: Russell King <rmk+kernel@arm.linux.org.uk>
    Cc: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>

--- include/linux/component.h
+++ include/linux/component.h
@@ -41,7 +41,7 @@ void component_del(struct device *, const struct component_ops *);
 int component_bind_all(struct device *master, void *master_data);
 void component_unbind_all(struct device *master, void *master_data);
 
-struct master;
+struct aggregate_device;
 
 /**
  * struct component_master_ops - callback for the aggregate driver
